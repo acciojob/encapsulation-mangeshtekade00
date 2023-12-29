5701735a6a603a622a79336d3ae1c43a2866e30e
mangeshtekade00 <mangeshtekade00@gmail.com>
2023-12-29 14:41:55 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String [] args){
+        RWOnly  obj = new RWOnly();
+        obj.setName("Mangu Changu");
+        System.out.println(obj.getName());
+    }
+
+
 }
